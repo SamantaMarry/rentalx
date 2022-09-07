052b85fe9cd977fcc02ba139b1cf82a95a4a6064
Samanta Marry <samantamaarry@gmail.com>
2022-09-07 19:16:14 -0300

    Change version typeORM and create migrations category

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,14 +1,13 @@
-import { DataSource } from "typeorm";
+import { createConnection, getConnectionOptions } from 'typeorm';
 
-const dataSource = new DataSource({
-    type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: "docker",
-    password: "ignite",
-    database: "rentx",
+interface IOptions {
+  host: string;
+}
 
-
-})
-
-dataSource.initialize()
+getConnectionOptions().then(options => {
+  const newOptions = options as IOptions;
+  newOptions.host = 'database'; //Essa opção deverá ser EXATAMENTE o nome dado ao service do banco de dados
+  createConnection({
+    ...options,
+  });
+});
