9a1722a2ff35480882ade9d9ca3ba0766ff68cee
Samanta Marry <samantamaarry@gmail.com>
2022-09-27 09:21:19 -0300

    prettier

--- src/module/accounts/entities/repositories/implementation/UsersRepository.ts
+++ src/module/accounts/entities/repositories/implementation/UsersRepository.ts
@@ -1,36 +1,24 @@
-
-import { User } from '../../User';
-import { IUsersRepository } from '../IUsersRepository';
-import { getRepository, Repository } from 'typeorm';
-import { IUsersDTO } from '../../../dtos/IUsersDTO';
-
+import { User } from "../../User";
+import { IUsersRepository } from "../IUsersRepository";
+import { getRepository, Repository } from "typeorm";
+import { IUsersDTO } from "../../../dtos/IUsersDTO";
 
 class UsersRepository implements IUsersRepository {
+  private repository: Repository<User>;
 
-    private repository: Repository<User>;
+  constructor() {
+    this.repository = getRepository(User);
+  }
 
-    constructor(){
+  async create(data: IUsersDTO): Promise<void> {
+    const user = this.repository.create(data);
 
-        this.repository = getRepository(User);
-    }
+    await this.repository.save(user);
+  }
+  async findByEmail(email: string): Promise<User> {
+    const user = this.repository.findOne({ email });
 
-    
-   async create(data: IUsersDTO): Promise<void> {
-
-        const user = this.repository.create(data);
-
-        await this.repository.save(user);
-
-    
-    }
-    async findByEmail(email: string): Promise<User> {
-
-        const user = this.repository.findOne({ email });
-
-        return user;
-    }
-
-
-
+    return user;
+  }
 }
-export { UsersRepository }
+export { UsersRepository };
